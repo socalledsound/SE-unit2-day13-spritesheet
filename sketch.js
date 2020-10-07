c47559ac275fe33bc5a3e3394a6399576d2dfc21
Chris Kubick <socalledsound@gmail.com>
2020-10-06 18:43:26 -0700

    row of abstractions

--- sketch.js
+++ sketch.js
@@ -13,33 +13,46 @@ let spritedata;
 
 let animation = [];
 
-let horses = [];
+// let abstract;
+let abstractions = [];
 
 function preload() {
-  spritedata = loadJSON('horse/horse.json');
-  spritesheet = loadImage('horse/horse.png');
+  spritedata = loadJSON('abstract/abstract.json');
+  spritesheet = loadImage('abstract/abstract.png');
 }
 
 function setup() {
   createCanvas(640, 480);
+  
+  //this is the array from our JSON file
   let frames = spritedata.frames;
+
+//now we loop over the array data and load each image into an array called animation
   for (let i = 0; i < frames.length; i++) {
     let pos = frames[i].position;
     let img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
     animation.push(img);
   }
 
+  //make an instance of the Sprite class
+  // abstract = new Sprite(animation, 100, 100, 320, 0.25);
+
   for (let i = 0; i < 5; i++) {
-    horses[i] = new Sprite(animation, 0, i * 75, random(0.1, 0.4));
+    abstractions[i] = new Sprite(animation, i * 100, 100, 160, random(0.1, 0.4));
   }
 }
 
 function draw() {
   background(0);
 
-  for (let horse of horses) {
-    horse.show();
-    horse.animate();
+  //use the sprite class to show and animate the spritesheet
+  // abstract.show();
+  // abstract.animate();
+
+
+  for (let abstraction of abstractions) {
+    abstraction.show();
+    abstraction.animate();
   }
 
   // image(animation[frameCount % animation.length], 0, 0);
--- sprite.js
+++ sprite.js
@@ -9,7 +9,7 @@
 // https://youtu.be/3noMeuufLZY
 
 class Sprite {
-  constructor(animation, x, y, speed) {
+  constructor(animation, x, y, size, speed) {
     this.x = x;
     this.y = y;
     this.animation = animation;
@@ -17,22 +17,26 @@ class Sprite {
     this.len = this.animation.length;
     this.speed = speed;
     this.index = 0;
+    this.size = size;
   }
 
 
   //notice the use of modulo to keeop the value of index in range of the number of images, ie this.len.....
   show() {
     let index = floor(this.index) % this.len;
-    image(this.animation[index], this.x, this.y);
+    image(this.animation[index], this.x, this.y, this.size, this.size);
   }
 
   animate() {
     //increment the index and the position of x
     this.index += this.speed;
-    this.x += this.speed * 15;
-    //if this.x exceeds the screen, send it back to the beginning of the screen.....
-    if (this.x > width) {
-      this.x = -this.w;
-    }
+
   }
 }
+
+
+    // this.x += this.speed * 15;
+    //if this.x exceeds the screen, send it back to the beginning of the screen.....
+    // if (this.x > width) {
+    //   this.x = -this.w;
+    // }
